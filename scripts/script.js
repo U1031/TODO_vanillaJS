df9e81f2538144b5343408157fe4f37057a6c4f3
Yu Jin Seong <abc9816@ajou.ac.kr>
2017-07-11 18:44:07 +0900

    adding li and checkbox controller

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,12 @@ var todo_list = document.querySelector('.todo_list');
 var all_click = document.getElementById('all_click');
 var btn_div = document.querySelector('.btn_div');
 var item_count = document.getElementById('item_count');
+var main_todo = document.querySelector('.main_todo');
+
+
+var p = []
+
+var list_cb = []
 
 
 var todoStorage = [
@@ -29,21 +35,34 @@ var completed_id_count = 0;
 
 
 clear_completed.onclick = function(){
+    alert(list_cb[1].checked);
+    delete_todo();
     alert("clear the completed!");
 }
 
 input_bar.onkeydown = function(){
     if(event.keyCode == 13){
         if(input_bar.value != ''){
-            todoStorage[id_num].id = id_num + 1;
+            //todoStorage[id_num].id = id_num + 1;
             todoStorage[id_num].title = input_bar.value;
             todoStorage[id_num].completed = false;
-            var p = document.createElement('li');
-            p.innerHTML = todoStorage[id_num].title;
-            todo_list.appendChild(p);
+
+            
+            p[id_num] = document.createElement('li');
+            list_cb[id_num] = document.createElement('input');
+            
+
+            list_cb[id_num].type = "checkbox";
+            p[id_num].innerHTML = todoStorage[id_num].title;
+
+            todo_list.appendChild(list_cb[id_num]);
+            todo_list.appendChild(p[id_num]);
+
             id_num++;
-            block_none();
             count_active_completed();
+            block_none();
+
+            input_bar.value = '';
         }else{
         }
     }
@@ -63,13 +82,15 @@ show_completed.onclick = function(){
 
 //브라우져에 보여주고 안보여주고를 결정하는 Function
 function block_none(){
-    item_count.innerHTML = id_num + " items left";
-    if(id_num != 0){
+    item_count.innerHTML = active_id_count + " items left";
+    if((active_id_count+completed_id_count) != 0){
         all_click.style.display = "block";
         btn_div.style.display = "block";
-    }else if(id_num == 0){
+        main_todo.style.display = "block";
+    }else if((active_id_count+completed_id_count) == 0){
         all_click.style.display = "none";
         btn_div.style.display = "none";
+        main_todo.style.display = "none";
     }
 }
 
@@ -89,8 +110,12 @@ function count_active_completed(){
     }
 }
 
-/*
-        var p = document.createElement('li');
-        p.innerHTML = todoStorage[0].title;
-        document.body.appendChild(p);
-*/
+//Clear Completed 눌렀을때 todoStorage에서 지우는 Function
+function delete_todo(){
+    for(var i = 0; i < todoStorage.length; i++){
+        if(todoStorage[i].completed == true){
+            todoStorage[i].title = '';
+            todoStorage[i].completed = false;
+        }
+    }
+}
